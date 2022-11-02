e6a9d2b8517e3bf97b39625c69f2995da4a8b986
Artem LAPOV <artemlapov@me.com>
2022-11-02 14:43:31 +0100

    Initial Commit

---
+++ HW 3_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 3_1
+//
+//  Created by Artem Lapov on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
